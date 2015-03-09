3d30a7c1fd460d1bb95967ceb0ce8487024353f7
Andres Giraldo <actorgiraldo@gmail.com>
2015-03-09 14:18:20 -0700

    new update

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -12,6 +12,12 @@ game.PlayerEntity = me.Entity.extend({
         }]);
     
     this.body.setVelocity(5, 25);
+    me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+    
+    this.renderable.addAnimation("idle", [78]);
+    this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125]);
+    
+    this.renderable.setCurrentAnimation("idle");
     
     },
     
@@ -21,11 +27,23 @@ game.PlayerEntity = me.Entity.extend({
             //set velocity() and multiplying it by me.timer.tick.
             //me.timer.tick makes the movement look smooth
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+            this.flipX(true);
         }else {
-            this.body.vel.x =0;
+            this.body.vel.x = 0;
+        }
+        
+        if(this.body.vel.x !== 0) {
+            if(!this.renderable.isCurrentAnimation("walk")) {
+                this.renderable.setCurrentAnimation("walk");
+                
+            }
+        }else {
+            this.renderable.setCurrentAnimation("idle");
         }
         
         this.body.update(delta);
+        
+        this._super(me.Entity, "update", [delta]);
         return true;
     }
 });
@@ -48,11 +66,16 @@ game.PlayerBaseEntity = me.Entity.extend({
         this.body.onCollision = this.onCollision.bind(this);
         
         this.type = "PlayerBaseEntity";
+        
+        this.renderable.addAnimation("idle", [0]);
+            this.renderable.addAnimation("broken", [1]);
+            this.renderable.setCurrentAnimation("idle")
     },
     
     update:function(delta) {
         if(this.health<=0){
             this.broken = true;
+              this.renderable.setCurrentAnimation("broken");
         }
         this.body.update(delta);
         
@@ -84,11 +107,16 @@ game.EnemyBaseEntity = me.Entity.extend({
         this.body.onCollision = this.onCollision.bind(this);
         
         this.type = "EnemyBaseEntity";
+        
+            this.renderable.addAnimation("idle", [0]);
+            this.renderable.addAnimation("broken", [1]);
+            this.renderable.setCurrentAnimation("idle");
     },
     
     update:function(delta) {
         if(this.health<=0){
             this.broken = true;
+            this.renderable.setCurrentAnimation("broken");
         }
         this.body.update(delta);
         
