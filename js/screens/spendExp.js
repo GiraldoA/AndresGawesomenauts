29d836b5133e47ceaf07762f67d4c6e46c2386d5
Andres Giraldo <actorgiraldo@gmail.com>
2015-04-14 13:46:57 -0700

    new update

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -1,36 +1,21 @@
-game.TitleScreen = me.ScreenObject.extend({
+game.SpendExp = me.ScreenObject.extend({
 	/**	
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('exp-screen')), -10); // TODO
          
                 
                 me.game.world.addChild(new (me.Renderable.extend({
                     init: function() {
                         this._super(me.Renderable, 'init', [380, 340, 300, 50]);
                         this.font = new me.Font("Arial", 46, "white");
-                        me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true)
                     },
                     
                     draw: function(renderer) {
-                        this.font.draw(renderer.getContext(), "START A NEW GAME", this.pos.x    , this.pos.y)
-                    },
-                    
-                    update: function(dt) {
-                        return true;
-                    },
-                    
-                    newGame: function() {
-                        me.input.releasePointerEvent('pointerdown', this);
-                        me.save.remove('exp');
-                        me.save.remove('exp1');
-                        me.save.remove('exp2');
-                        me.save.remove('exp3');
-                        me.save.remove('exp4');
-                        me.save.add({exp: 0, exp1: 0, exp2: 0, exp3: 0, exp4: 0});
-                        me.state.change(me.state.PLAY);
+                        this.font.draw(renderer.getContext(), "Spend", this.pos.x    , this.pos.y)
                     }
+                  
                 })));
                
 	},
