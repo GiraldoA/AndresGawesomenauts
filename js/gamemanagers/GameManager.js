ee28e01af218da19a388f2c04fa6f29e726b0c46
Andres Giraldo <actorgiraldo@gmail.com>
2015-04-28 21:50:14 -0700

    new update

--- js/gamemanagers/GameManager.js
+++ js/gamemanagers/GameManager.js
@@ -10,9 +10,11 @@ game.ExperienceManager = Object.extend({
         //if the player wins the game he then gets 10 experience
         if (game.data.win === true && !this.gameover) {
             this.gameOver(true);
+            alert("YOU WIN!!!");
             //if the player losses the game he gets one experience
         } else if (game.data.win === false && !this.gameover) {
             this.gameOver(false);
+            alert("YOU LOSE!");
         }
         return true;
     },
@@ -27,6 +29,33 @@ game.ExperienceManager = Object.extend({
         me.save.exp = game.data.exp;
         //for testing purposes only
         me.save.exp2 = 4;
+
+
+
+
+        $.ajax({
+            type: "POST",
+            url: "php/controller/save-user.php",
+            data: {
+                exp: game.data.exp,
+                exp1: game.data.exp1,
+                exp2: game.data.exp2,
+                exp3: game.data.exp3,
+                exp4: game.data.exp4,
+            },
+            dataType: "text"
+        })
+                .success(function(response) {
+                    if (response === "true") {
+                        me.state.change(me.state.MENU);
+                    } else {
+                        alert(response);
+                    }
+                })
+                .fail(function(response) {
+                    alert("Fail");
+                });
+
     }
 });
 
